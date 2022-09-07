3ea106ffa3480162bb3318b95efad8cda9d44924
idkvaiop <104100121+idkvaiop@users.noreply.github.com>
2022-09-07 11:50:08 +0530

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -119,7 +119,7 @@ HELP_STRINGS = f"""
   
 DONATE_STRING = """Hello, Firstly thanks for thinking to donate for my hardwork !
 Helping/ Donation is not always financial... You can help in many other ways like promoting our bot in more and more chats.
-You can contact [Akhil 🇮🇳](tg://user?id={2102783671}) for more information !"""
+You can contact us in support group  for more information !"""
 
 
 IMPORTED = {}
