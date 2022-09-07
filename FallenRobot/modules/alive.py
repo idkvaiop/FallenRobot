f19c7ae333f7c97afd3bfc36831c4d24ca356c61
idkvaiop <104100121+idkvaiop@users.noreply.github.com>
2022-09-07 12:03:25 +0530

    Update alive.py

--- FallenRobot/modules/alive.py
+++ FallenRobot/modules/alive.py
@@ -9,27 +9,26 @@ from FallenRobot import OWNER_USERNAME, SUPPORT_CHAT, dispatcher
 from FallenRobot import telethn as tbot
 from FallenRobot.events import register
 
-PHOTO = [
-    "https://telegra.ph/file/6a21ea6677342f43b363e.jpg",
-    "https://telegra.ph/file/40eb1ed850cdea274693e.jpg",
+ANIMATION = [
+    "https://telegra.ph/file/9d2fbe40731e96d0f7b25.mp4",
+    "https://telegra.ph/file/9d2fbe40731e96d0f7b25.mp4",
 ]
 
 
 @register(pattern=("/alive"))
 async def awake(event):
-    TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ {dispatcher.bot.first_name}**\n━━━━━━━━━━━━━━━━━━━\n\n"
-    TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [𝝙𝗡𝗢𝗡𝗬𝗠𝗢𝗨𝗦](https://t.me/{OWNER_USERNAME})** \n\n"
-    TEXT += f"» **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n\n"
-    TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
-    TEXT += f"» **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n━━━━━━━━━━━━━━━━━\n\n"
+    TEXT = f"**Hello​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nI am{dispatcher.bot.first_name}**\n☆━━━━━━━━━━━━━━━━☆\n\n"
+    TEXT += f"**Creator :** [Akhil 🇮🇳](tg://user?id={2102783671})** \n\n"
+    TEXT += f"**Library Version:** `{telever}` \n\n"
+    TEXT += f"**Telethon Version :** `{tlhver}` \n\n"
+    TEXT += f"**Pyrogram Version :** `{pyrover}` \n━━━━━━━━━━━━━━━━━\n\n"
     BUTTON = [
         [
-            Button.url("ʜᴇʟᴘ​", f"https://t.me/{dispatcher.bot.username}?start=help"),
-            Button.url("sᴜᴘᴘᴏʀᴛ​", f"https://t.me/{SUPPORT_CHAT}"),
+            Button.url("Help​", f"https://t.me/{dispatcher.bot.username}?start=help"),
         ]
     ]
-    ran = random.choice(PHOTO)
+    ran = random.choice(ANIMATION)
     await tbot.send_file(event.chat_id, ran, caption=TEXT, buttons=BUTTON)
 
 
-__mod_name__ = "Aʟɪᴠᴇ"
+__mod_name__ = "Alive"
